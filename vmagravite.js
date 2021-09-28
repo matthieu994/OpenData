f36fe37c2c890470974a7d12f5bff603bf4f0984
Matthieu <matthieu.petit.contact@gmail.com>
2021-09-28 17:17:13 +0200

    update vma chart instead of destroy

--- vmagravite.js
+++ vmagravite.js
@@ -1,80 +1,95 @@
-var vma_gravite_chart;
-function vmaGraviteChart(dept, name) {
-	const DATA_COUNT = 4;
-	promises[4].then(function (values) {
-		var vma50 		= JSON.parse(values[dept]["50kmh"]);
-		var vma80to90 	= JSON.parse(values[dept]["80to90kmh"]);
-		var vma110 		= JSON.parse(values[dept]["110kmh"]);
-		var vma130 		= JSON.parse(values[dept]["130kmh"]);
+let vma_gravite_chart, all_values_vma_bar;
 
-		var indemmes 	= [vma50[0], vma80to90[0], vma110[0], vma130[0]];
-		var deces 		= [vma50[1], vma80to90[1], vma110[1], vma130[1]];
-		var graves 		= [vma50[2], vma80to90[2], vma110[2], vma130[2]];
-		var legers 		= [vma50[3], vma80to90[3], vma110[3], vma130[3]];
-		console.log(vma50);
-		console.log(vma80to90);
-		console.log(vma110);
-		console.log(vma130);
+function vmaGraviteChart(values) {
+  const DATA_COUNT = 4;
+  all_values_vma_bar = values;
 
-		const labels = ["50km/h", "80-90km/h", "110km/h", "130km/h"];
-		const data = {
-		  	labels: labels,
-		  	datasets: [
-		  	  	{
-		  	  	  	label: 'Indemmes',
-		  	  	  	data: indemmes,
-		  	  	  	backgroundColor: '#2c9c69',
-		  	  	},
-		  	  	{
-		  	  	  	label: 'Blessés légers',
-		  	  	  	data: legers,
-		  	  	  	backgroundColor: '#dbba34',
-		  	  	},
-		  	  	{
-		  	  	  	label: 'Blessés graves',
-		  	  	  	data: graves,
-		  	  	  	backgroundColor: '#c62f29',
-		  	  	},
-		  	  	{
-		  	  	  	label: 'Décès',
-		  	  	  	data: deces,
-		  	  	  	backgroundColor: '#000000',
-		  	  	}
-		  	]
-		};
+  let [indemnes, deces, graves, legers] = getVMADataForDept(95);
 
+  const labels = ["50km/h", "80-90km/h", "110km/h", "130km/h"];
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: "Indemnes",
+        data: indemnes,
+        backgroundColor: "#2c9c69",
+      },
+      {
+        label: "Blessés légers",
+        data: legers,
+        backgroundColor: "#dbba34",
+      },
+      {
+        label: "Blessés graves",
+        data: graves,
+        backgroundColor: "#c62f29",
+      },
+      {
+        label: "Décès",
+        data: deces,
+        backgroundColor: "#000000",
+      },
+    ],
+  };
 
-		const config = {
-		  	type: 'bar',
-		  	data: data,
-		  	options: {
-		  	  	plugins: {
-		  	  	  	title: {
-		  	  	  	  display: true,
-		  	  	  	  text: 'Gravité des accidents en fonction de la VMA à '+name
-		  	  	  	},
-		  	  	},
-		  	  	responsive: true,
-		  	  	scales: {
-		  	  	  	x: {
-		  	  	  	  stacked: true,
-		  	  	  	},
-		  	  	  	y: {
-		  	  	  	  stacked: true
-		  	  	  	}
-		  	  	}
-		  	}
-		};
+  const config = {
+    type: "bar",
+    data: data,
+    options: {
+      plugins: {
+        title: {
+          display: true,
+          text: "Gravité des accidents en fonction de la VMA en FRANCE",
+        },
+      },
+      responsive: true,
+      scales: {
+        x: {
+          stacked: true,
+        },
+        y: {
+          stacked: true,
+        },
+      },
+    },
+  };
 
-	  	vma_gravite_chart = new Chart(document.getElementById('vmagravite'), config);
-	});
+  vma_gravite_chart = new Chart(document.getElementById("vmagravite"), config);
 }
+d3.csv("data/vmaGraviteData.csv").then((data) => vmaGraviteChart(data));
+
 function updateVmaGraviteChart(dept, dept_name) {
-  vma_gravite_chart.destroy();
+  let code = parseInt(dept);
+
   if (dept === null) {
-  	vmaGraviteChart(95, "FRANCE");
+    code = 95;
+    vma_gravite_chart.options.plugins.title.text =
+      "Gravité des accidents en fonction de la VMA en FRANCE";
   } else {
-  	vmaGraviteChart(dept, dept_name);
+    vma_gravite_chart.options.plugins.title.text =
+      "Gravité des accidents en fonction de la VMA à " + dept_name;
   }
+
+  let [indemnes, deces, graves, legers] = getVMADataForDept(code);
+  console.log(vma_gravite_chart.data.datasets);
+  vma_gravite_chart.data.datasets[0].data = indemnes;
+  vma_gravite_chart.data.datasets[1].data = legers;
+  vma_gravite_chart.data.datasets[2].data = graves;
+  vma_gravite_chart.data.datasets[3].data = deces;
+  vma_gravite_chart.update();
 }
-vmaGraviteChart(95, "FRANCE");
+
+function getVMADataForDept(dept) {
+  var vma50 = JSON.parse(all_values_vma_bar[dept]["50kmh"]);
+  var vma80to90 = JSON.parse(all_values_vma_bar[dept]["80to90kmh"]);
+  var vma110 = JSON.parse(all_values_vma_bar[dept]["110kmh"]);
+  var vma130 = JSON.parse(all_values_vma_bar[dept]["130kmh"]);
+
+  return [
+    [vma50[0], vma80to90[0], vma110[0], vma130[0]],
+    [vma50[1], vma80to90[1], vma110[1], vma130[1]],
+    [vma50[2], vma80to90[2], vma110[2], vma130[2]],
+    [vma50[3], vma80to90[3], vma110[3], vma130[3]],
+  ];
+}
